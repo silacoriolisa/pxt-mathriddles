3136175cf570fe92a22e7b7acbf4a737e8414d5e
silacoriolisa <135693825+silacoriolisa@users.noreply.github.com>
2024-11-18 10:28:37 +0100

    Aktualizacja main.ts

--- main.ts
+++ main.ts
@@ -2,82 +2,82 @@
 //% color="#AA278D" weight=100 block="Math Riddles"
 namespace MathRiddle{
 
+    export class Riddle{
+        riddle: string;
+        result: number;
+        answer: number;
+ 
+        //%block = "Create a riddle in %myRiddle"
+        askRiddle(): void{
+            let mathOp = randOp();
+            let operator, opA, opB;
 
-    //%block = "Ask a riddle"
-    export function askRiddle(): number{
-
-        let mathOp = randOp()
-        let operator, opA, opB, result
-        switch(mathOp){
-            case 1:
-                opA = randint(1, 15)
-                opB = randint(1, 15)
-                operator = "+"
-                result = opA + opB
-                break
-            case 2:
-                opA = randint(1, 15)
-                opB = randint(1, opA)
-                operator = "-"
-                result = opA - opB
-                break
-            case 3:
-                opA = randint(1, 10)
-                opB = randint(1, (10 - opA + 1))
-                operator = "*"
-                result = opA * opB
-                break
-            case 4:
-                
+            switch (mathOp) {
+                case 1:
+                    opA = randint(1, 15);
+                    opB = randint(1, 15);
+                    operator = "+";
+                    this.result = opA + opB;
+                    break;
+                case 2:
+                    opA = randint(1, 15);
+                    opB = randint(1, opA);
+                    operator = "-";
+                    this.result = opA - opB;
+                    break;
+                case 3:
+                    opA = randint(1, 10);
+                    opB = randint(1, (10 - opA + 1));
+                    operator = "*";
+                    this.result = opA * opB;
+                    break;
+                case 4:
+            }
+            this.riddle = (convertToText(opA) + operator + convertToText(opB));
         }
-        //show riddle
-        basic.showString(convertToText(opA) + operator + convertToText(opB))
-        basic.showString("?")
-        basic.pause(500)
-        basic.clearScreen()
-        return result
 
-    }
+        //%block = "Show %myRiddle"
+        showRiddle(): void{
+            basic.showString(this.riddle + "?");
+        }
 
-    //%block = "Wait for reply"
-    export function waitForReply(): number{
-        let tens, ones, result = 0
-           
-        while (!(input.buttonIsPressed(Button.A) && input.buttonIsPressed(Button.B))) {
-            basic.showNumber(tens)
-            if(input.buttonIsPressed(Button.A)){
-                if( tens < 9 ){
-                    tens++
-                }else{
-                    tens = 0
-                }
-            }else if(input.buttonIsPressed(Button.B)){
-                if (tens > 0) {
-                    tens--
-                } else {
-                    tens = 9
+        //%block = "Show answer to %myRiddle"
+        showAnswer(): void{
+            basic.showString(convertToText(this.answer));
+        }
+
+        //%block = "Wait for a reply to %myRiddle"
+        getReply(): void{
+        let tens, ones, result = 0;
+
+        tens = this.updateReply(tens);
+        basic.pause(200);
+        ones = this.updateReply(ones);
+        
+        this.result = tens * 10 + ones;
+        }
+
+        private updateReply(value: number): number{
+            while (!(input.buttonIsPressed(Button.A) && input.buttonIsPressed(Button.B))) {
+                basic.showNumber(value);
+                if (input.buttonIsPressed(Button.A)) {
+                    if (value < 9) {
+                        value++;
+                    } else {
+                        value = 0;
+                    }
+                } else if (input.buttonIsPressed(Button.B)) {
+                    if (value > 0) {
+                        value--;
+                    } else {
+                        value = 9;
+                    }
                 }
             }
+            return value
         }
-        while (!(input.buttonIsPressed(Button.A) && input.buttonIsPressed(Button.B))) {
-            basic.showNumber(ones)
-            if (input.buttonIsPressed(Button.A)) {
-                if (ones < 9) {
-                    ones++
-                } else {
-                    ones = 0
-                }
-            }else if (input.buttonIsPressed(Button.B)) {
-                if (ones > 0) {
-                    ones--
-                } else {
-                    ones = 9
-                }            
-            }
-        }
-        result = tens * 10 + ones
-        basic.showNumber(result)
-        return result
+
+    //end of class definition
     }
 
     function randOp(): number{
@@ -85,4 +85,17 @@ namespace MathRiddle{
         //Division needs to be thought through
         return randint(1, 3)
     }
+
+    //% block="NewRiddle"
+    //% blockSetVariable=myRiddle
+    export function createMyRiddle(): Riddle {
+        let myRiddle = new Riddle();
+        myRiddle.riddle = "";
+        myRiddle.result = 0;
+        myRiddle.answer = 0;
+        return myRiddle;
+    }
+
 }
+
+
