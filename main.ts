a2f6107578bf902daf50a8e3a79b4bb2efdfe6cb
silacoriolisa <135693825+silacoriolisa@users.noreply.github.com>
2024-11-18 08:16:08 +0100

    Aktualizacja main.ts

--- main.ts
+++ main.ts
@@ -5,23 +5,35 @@ namespace MathRiddle{
 
 
     //block = "Ask a riddle"
-    export function askRiddle(): boolean{
+    export function askRiddle(): void{
 
-        let opA = randint(1,15)
-        let opB = randint(1,15)
         let mathOp = randOp()
-
+        let operator, opA, opB
         switch(mathOp){
             case 1:
-            opA = Math.constrain(opA, 1, 10)
-            opB = Math.constrain(opB, 1, 10)
+                opA = randint(1, 15)
+                opB = randint(1, 15)
+                operator = "+"
+            case 2:
+                opA = randint(1, 15)
+                opB = randint(1, opA)
+                operator = "-"
+            case 3:
+                opA = randint(1, 10)
+                opB = randint(1, (10 - opA + 1))
+                operator = "*"
+            case 4:
+                
         }
+        //show riddle
+        
+        basic.showString(convertToText(opA) + operator + convertToText(opB))
 
-        return 0
     }
 
     function randOp(): number{
         // 1:"+", 2:"-", 3:"*", 4:"/"
-        return randint(1, 4)
+        //Division needs to be thought through
+        return randint(1, 3)
     }
 }
